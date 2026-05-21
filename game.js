0d849f8b943731d3bbb46e666845d43e85f54544
SathishJ292004 <sathishsj292004@gmail.com>
2026-05-21 08:31:02 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,12 @@
+let guess = 0;
+let secretNumber = 5;
+
+while (guess !== secretNumber) {
+    Guess = Number("guess the number")
+    if (guess == secretNumber) {
+        console.log("crt guess")
+    }
+    else {
+        console.log("try again")
+    }
+}
